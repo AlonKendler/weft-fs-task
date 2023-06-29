fad505ba6ed1ca8f5b13707ac21125399e88fe1b
alon <alonkender@gmail.com>
2023-06-29 21:55:11 +0300

    remove

--- src/server/dbClient.ts
+++ src/server/dbClient.ts
@@ -2,15 +2,6 @@
 import { sql } from '@vercel/postgres';
 
 export class PostgresClient {
-  // async fetchUsers(url: string) {
-  //   const response = await fetch(url);
-  //   if (!response.ok) {
-  //     throw new Error(`HTTP error! status: ${response.status}`);
-  //   }
-  //   const users = await response.json();
-  //   return users;
-  // }
-
   async getPostsByUserId(userId: string) {
     const result = await sql`SELECT * FROM posts WHERE userId = ${userId}`;
     return result.rows;
